b82511f4f44f7ce7051ee2c36229d3f440eda1c7
Developer4you <avervalpet@gmail.com>
2022-08-14 19:41:58 +0300

    feat: add routes

--- src/api/api.ts
+++ src/api/api.ts
@@ -3,30 +3,11 @@ import axios from "axios";
 
 const instance = axios.create({
     withCredentials: true,
-    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+    baseURL: '',
     headers:     {
-        "API-KEY": "f427df99-3f26-4d64-845e-4734f54c112c"
+        "API-KEY": ""
     }
 });
-
-
-export const usersAPI = {
-    getUsers(currentPage = 15, pageSize = 15) {
-        return instance.get(`users?page=${currentPage}&count=${pageSize}`)
-            .then(response => {
-                return response.data;
-            });
-    },
-    follow(userId:string) {
-        return instance.post(`follow/${userId}`)
-    },
-    unfollow(userId:string) {
-        return instance.delete(`follow/${userId}`)
-    },
-    getProfile(userId:string) {
-        return instance.get(`profile/` + userId);
-    }
-}
 
 export const authAPI = {
     me() {
